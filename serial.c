2328f0bcf3b11fa332990518737ef4c3db26c4c3
Wilson Wang <wilson97@linux3.cs.uchicago.edu>
2018-01-14 16:51:56 -0600

    4

--- serial.c
+++ serial.c
@@ -3,26 +3,40 @@
 #include <string.h>
 #include "serial.h"
 
-//helper function to tokenize a string
+// helper function to tokenize a string
 int* stringToArr(char* str, int N)
 {
    char* c;
    int* outArr = (int*) malloc(N * sizeof(int));
    int index = 0; 
    c = strtok(str, " ");
-   //char cp[1000];
    while (c != NULL)
    {
-      //strcpy(cp, c);
       outArr[index] = atoi(c); 
       c = strtok(NULL, " ");
-      //printf("%s\n", outArr[0]); 
       index += 1;
    }
    return outArr;
 }
 
-// does one thing: returns the newly created adjacency list
+// returns the size - from the first line of the text file
+int getSize(char* fileName)
+{
+   int size = 0;
+   FILE *file = fopen(fileName, "r");
+   if (file != NULL)
+   {
+      char line[100];
+      if (fgets(line, sizeof(line), file) != NULL)
+      {
+         size = atoi(line);   
+      }
+   }
+   fclose(file);
+   return size;
+}
+
+// returns the newly created adjacency list
 int** readData(char* fileName)
 {
    int size = 0;
@@ -36,10 +50,6 @@ int** readData(char* fileName)
       {
          size = atoi(line);
          adjList = (int**) malloc(size * sizeof(int *));
-	 for (int i = 0; i < size; i++)
-         {
-            adjList[i] = (int*) malloc(size * sizeof(int));
-         } 
       }
 
       // now load the text file into adjList 
@@ -49,7 +59,6 @@ int** readData(char* fileName)
          adjList[index] = stringToArr(line, size);
          index += 1;
       }
-      printf("TRY: %d\n", adjList[2][3]);
 
    }
    fclose(file);
@@ -64,12 +73,54 @@ int writeData(char* fileName, int** minList, int N)
       printf("Something bad happened when opening file\n");
       return -1;
    }
-      
+   for (int i = 0; i < N; i++)
+   {
+      for (int j = 0; j < N; j++)
+      {
+         fprintf(file, "%d ", minList[i][j]);      
+      }
+      fprintf(file, "%s", "\n");
+   }
+   fclose(file);   
+   return 0;
 }
 
-int solvePaths(int** adjList, int** minList)
+// follow the wikipedia algorithm
+int** solvePaths(int** adjList, int N)
 {
-   return 0;
+   if (N == 0)
+   {
+      printf("N is 0\n");
+      return NULL;
+   }
+   
+   // copy adjacency list into dist, we assume adjList already has 0 for dist[k][k] and 10^7 for no edge
+   int** dist = (int**) malloc(N * sizeof(int *));
+   for (int i = 0; i < N; i++)
+   {  
+      dist[i] = (int*) malloc(N * sizeof(int));
+      for (int j = 0; j < N; j++)
+      {
+         dist[i][j] = adjList[i][j];
+      } 
+   }
+
+   printf("Doing something!!!\n");
+   for (int k = 0; k < N; k++)
+   {
+      for (int i = 0; i < N; i++)
+      {
+         for (int j = 0; j < N; j++)
+         {
+            //(1,2) (1,0), (0,2)
+            if (dist[i][j] > dist[i][k] + dist[k][j])
+            {
+               dist[i][j] = dist[i][k] + dist[k][j];
+            } 
+         }
+      }
+   }
+   return dist;
 }
 
 int main()
@@ -78,18 +129,26 @@ int main()
    
    //int** adjList = (int**) malloc(N * sizeof(int *));
    printf("Testing part 1:\n");
+   /*
    char str[] = "5 6 7 8 9";
    int* t1 = stringToArr(str, 5);
    for (int i = 0; i < 5; i++)
    {
       printf("%d\n", t1[i]);
-   }
+   }*/
 
    printf("Testing part 2:\n");
    int** t2 = NULL;
    //int size;
-   t2 = readData("test.txt"); 
-   printf("Size: %lu\n", sizeof(t2[0]));
-   printf("%d\n", t2[2][3]);
+   t2 = readData("1.txt");
+   int size = getSize("1.txt"); 
+   printf("Size: %d\n", size);
+   //printf("Size: %lu\n", sizeof(t2[0]));
+   //printf("%d\n", t2[2][3]);
+
+   printf("Testing part 3:\n");
+   int** t3 = NULL;
+   t3 = solvePaths(t2, size);
+   writeData("output.txt", t3, size);
    return(0);
 }
--- serial.h
+++ serial.h
@@ -1,8 +1,9 @@
 #ifndef serial_h
 #define serial_h
- 
+
+int getSize(char* fileName); 
 int** readData(char* fileName);
 int writeData(char* fileName, int** minList, int N);
-int solvePaths(int** adjList, int** minList);
+int** solvePaths(int** adjList, int N);
  
 #endif
